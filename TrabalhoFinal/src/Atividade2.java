c958a78058135aa47ea3f841bd5d6a8d37af3e80
Renan Costa <91814882+renanzitoo@users.noreply.github.com>
2022-07-06 11:43:07 -0300

    Add files via upload

---
+++ TrabalhoFinal/src/Atividade2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Atividade2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String frase, fraseI;
+		int cont, i;
+		System.out.println("Digite a frase");
+		frase = sc.nextLine();
+		cont = frase.length();
+		for (i=cont-1;i>=0;i--) {
+			System.out.print(frase.charAt(i));
+			
+		}
+		
+	}
+
+}
---
+++ TrabalhoFinal/src/Atividade5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;	
+public class Atividade5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		double kwpreco, salMin, kwprecDesc, kwprecoUn;
+		int kwUsado;
+			System.out.println("Digite a quantidade de KW usados");
+			kwUsado = sc.nextInt();
+			System.out.println("Digite o salario minimo");
+			salMin = sc.nextDouble();
+			kwpreco = (kwUsado/100) * (salMin/7);
+			System.out.println("O preço a ser pago"+kwpreco);
+			kwprecoUn = kwpreco/kwUsado;
+			System.out.println("O valor da unidade do KW"+kwprecoUn);
+			kwprecDesc = kwpreco - (kwpreco/0.1);
+			System.out.println("O valor com desconto"+kwprecDesc);
+			
+		
+	}
+}
